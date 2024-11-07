3aacb4c75eddfd8bb4ebce22530d1fc03785b163
dimas <dimasixc22@gmail.com>
2024-11-07 09:00:07 +0700

    rain amount 8 kyu

---
+++ rainAmount.js
@@ -0,0 +1,9 @@
+function rainAmount(m) {
+
+  if ( mm < 40 ) {
+    return `You need to give your plant ${40 - mm}mm of water`;
+  } else if ( mm > 100 ){
+    return 'Your plant has had more than enough water for today';
+  };
+  
+};
